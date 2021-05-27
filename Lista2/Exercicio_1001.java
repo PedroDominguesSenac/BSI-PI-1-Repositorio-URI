170a20421d95207137b0520eabf4256389f3088b
PedroDominguesSenac <senac.sp.pedro.domingues.com>
2021-05-26 23:50:52 -0300

    Pedro Domingues

---
+++ Lista2/Exercicio_1001.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista2;
+
+import java.util.Scanner;
+
+public class Exercicio_1001 {
+    
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+        int A = leitura.nextInt();
+        int B = leitura.nextInt();
+        int X = A + B;
+        System.out.println("X = "+ X);
+    
+    }
+}
